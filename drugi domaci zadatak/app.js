65c45660448ffa01b8a18e0c127b4ca0a4c47c0b
dautovicsaid <83250294+dautovicsaid@users.noreply.github.com>
2022-06-04 22:11:36 +0200

    Homework update

--- drugi domaci zadatak/app.js
+++ drugi domaci zadatak/app.js
@@ -3,7 +3,7 @@ const createMovieForm = document.getElementById("createMovieForm");
 
 const movies = [
   {
-    watched: true,
+    watched: false,
     title: "Interstellar",
     year: 2014,
     country: "SAD",
@@ -18,6 +18,14 @@ const movies = [
     note: "tekst",
     actors: ["Leonardo di Caprio", "Chuck Aule"],
   },
+  {
+    watched: false,
+    title: "The Invisible Guest",
+    year: 2016,
+    country: "SAD",
+    note: "tekst",
+    actors: ["Ana Wagener", "Tomas Garido"],
+  },
 ];
 
 function showMovies() {
@@ -31,7 +39,7 @@ function showMovies() {
 function movieRow(movie, index) {
   let watchedCheckboxChecked = movie.watched ? "checked" : "";
 
-  return `<tr>
+  return `<tr id="row${index}" class="notWatched">
     <td><input type="checkbox" ${watchedCheckboxChecked} onchange="setWatched(event, ${index})"/></td>
     <td>${movie.title}</td>
     <td>${movie.year}</td>
@@ -43,53 +51,112 @@ function movieRow(movie, index) {
 
 function setWatched(event, index) {
   let isChecked = event.target.checked;
+  let movieRow = document.getElementById(`row${index}`);
 
   movies[index].watched = isChecked;
-  console.log(movies);
+  movieRow.removeAttribute("class");
+
+  if (movies[index].watched) {
+    movieRow.classList.add("watched");
+  } else {
+    movieRow.classList.add("notWatched");
+  }
 }
 
 function addMovie(event) {
   event.preventDefault();
 
-  let movie = {
-    watched: false,
-    title: document.getElementById("title").value,
-    year: document.getElementById("year").value,
-    country: document.getElementById("country").value,
-    note: document.getElementById("note").value,
-    actors: document.getElementById("actors").value.split(","),
+  let formData = {
+    title: document.getElementsByName("title")[0].value,
+    year: document.getElementsByName("year")[0].value,
+    country: document.getElementsByName("country")[0].value,
+    note: document.getElementsByName("note")[0].value,
+    actors: document.getElementsByName("actors")[0].value,
   };
 
-  if (!validateMovie(movie)) {
+  if (!validateMovieFormData(formData)) {
     return;
   }
+
+  let movie = {
+    watched: false,
+    title: formData.title,
+    year: parseInt(formData.year),
+    country: formData.country,
+    note: formData.note,
+    actors: formData.actors.split(","),
+  };
 
   movies.push(movie);
   // moviesWrapper.innerHTML = moviesWrapper.innerHTML + movieRow(movie);
   showMovies();
+
+  createMovieForm.reset();
+  closeCreateMovieModal();
 }
 
-function validateMovie(movie) {
-  let errors = {};
+function validateMovieFormData(movie) {
+  let errors = {
+    title: undefined,
+    year: undefined,
+    country: undefined,
+    note: undefined,
+    actors: undefined,
+  };
 
-  if (movieData.title === "Greska") {
-    errors.title = "Mora imati preko 10 karaktera";
+  if (movie.title.length < 1) {
+    errors.title = "Morate unijeti naziv filma";
   }
 
-  if (movieData.year === 0) {
-    errors.year = "Godina imati preko 10 karaktera";
+  if (isNaN(movie.year)) {
+    errors.year = "Film mora biti broj";
+  } else if (!(parseInt(movie.year) > 1930 && parseInt(movie.year) < 2021)) {
+    errors.year = "Film mora biti napravljen u periodu od 1930-2021";
   }
 
-  if (Object.keys(errors).length > 0) {
-    displayCreateMovieErrors(errors);
-    return false;
+  if (movie.actors === "") {
+    errors.actors = "Film mora imati makar 1 glumca";
   }
 
+  for (let property of Object.keys(errors)) {
+    if (!(errors[property] === undefined)) {
+      displayCreateMovieErrors(errors);
+      return false;
+    }
+  }
+  // if (Object.keys(errors).length > 0) {
+  //   displayCreateMovieErrors(errors);
+  //   return false;
+  // }
   return true;
 }
 
 function displayCreateMovieErrors(errors) {
-  for (let [key, value] of errors) {
-    console.log(key, value);
+  for (let property of Object.keys(errors)) {
+    let errorMessage = errors[property];
+    let errorSpan = document.getElementById(`error-${property}`);
+
+    if (!(errorMessage == undefined)) {
+      errorSpan.removeAttribute("class");
+      errorSpan.classList.add("d-inline", "invalid-feedback");
+      errorSpan.innerHTML = errorMessage;
+    } else {
+      errorSpan.innerHTML = "";
+      errorSpan.removeAttribute("class");
+      errorSpan.classList.add("d-none");
+    }
+    // span#error-${property}.innerHtml = errorMEssage;
   }
+}
+
+function showCreateMovieModal() {
+  document.getElementById("backdrop").style.display = "block";
+  document.getElementById("createMovieModal").style.display = "block";
+  document.getElementById("createMovieModal").classList.add("show");
+}
+
+function closeCreateMovieModal() {
+  document.getElementById("backdrop").style.display = "none";
+  document.getElementById("createMovieModal").style.display = "none";
+  document.getElementById("createMovieModal").classList.remove("show");
 }
